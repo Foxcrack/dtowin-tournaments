e9477d50c3b10f7f728d953e0e64ec361a6a4270
Foxcrack <andresfelipe1v2c@gmail.com>
2025-05-05 00:17:17 -0500

    v0.0.0.4
    calculatorrsssss!!!!

--- secciones/index-calculadoras-folder/calculadoras-index.js
+++ secciones/index-calculadoras-folder/calculadoras-index.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/fisica-index.html";
     });
 
-    document.getElementById("Dirigir_a_CalculadorasIndex").addEventListener("click", () => {
-        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/fisica-index.html";
+    document.getElementById("Dirigir_a_QuimicaIndex").addEventListener("click", () => {
+        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/quimica-folder/quimica.html";
     });
 
 });
