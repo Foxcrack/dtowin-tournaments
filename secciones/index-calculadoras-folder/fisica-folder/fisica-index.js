08477ee657a8587cff59a1eb9d7209ab9d6c4cc7
Foxcrack <andresfelipe1v2c@gmail.com>
2025-05-04 23:59:23 -0500

    v0.0.4
    calculators wujuuuuuuu >:3

---
+++ secciones/index-calculadoras-folder/fisica-folder/fisica-index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("Dirigir_a_Mortero").addEventListener("click", () => {
+        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/mortero-folder/mortero.html";
+    });
+
+    document.getElementById("Dirigir_a_FisicaIndex").addEventListener("click", () => {
+        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/fisica-index.html";
+    });
+
+    document.getElementById("Dirigir_a_CalculadorasIndex").addEventListener("click", () => {
+        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/fisica-index.html";
+    });
+
+});
