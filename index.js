5e607b6f9adfb547e9785eb6ed4c530777081db3
Foxcrack <andresfelipe1v2c@gmail.com>
2025-05-04 23:51:51 -0500

    v0.0.4
    Calculadora de mortero activa wuuuujuuuuuuu

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("Dirigir_a_LeaderboardsIndex").addEventListener("click", () => {
         window.location.href = "secciones/index-leaderboards-folder/leaderboard-completo.html";
     });
+
+    document.getElementById("Dirigir_a_CalculadorasIndex").addEventListener("click", () => {
+        window.location.href = "secciones/index-calculadoras-folder/calculadoras-index.html";
+    });
+
+
+
+    document.getElementById("Dirigir_a_e621Index").addEventListener("click", () => {
+        window.location.href = "e621.html";
+    });
 });
---
+++ secciones/index-calculadoras-folder/calculadoras-index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("Dirigir_a_MatematicasIndex").addEventListener("click", () => {
+        window.location.href = "/secciones/index-torneos-folder/index-torneos.html";
+    });
+
+    document.getElementById("Dirigir_a_FisicaIndex").addEventListener("click", () => {
+        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/fisica-index.html";
+    });
+
+    document.getElementById("Dirigir_a_CalculadorasIndex").addEventListener("click", () => {
+        window.location.href = "/secciones/index-calculadoras-folder/fisica-folder/fisica-index.html";
+    });
+
+});
