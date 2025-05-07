654a61473fa251cdc8fef074426af6948dacf8ab
Foxcrack <andresfelipe1v2c@gmail.com>
2025-05-06 20:37:40 -0500

    jeje

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
+import { auth, loginWithGoogle, getUserProfile, logoutUser } from '../firebase.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
+
+const googleLoginBtn = document.getElementById('googleLogin');
+const logoutBtn = document.getElementById('logoutBtn');
+const userDataDiv = document.getElementById('userData');
+const adminPanelBtn = document.getElementById('adminPanel');
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("Dirigir_a_TorneosIndex").addEventListener("click", () => {
         window.location.href = "index-torneos.html";
@@ -21,3 +29,21 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "e621.html";
     });
 });
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      try {
+        const profile = await getUserProfile(user.uid);
+  
+        // Mostrar botón de e621 solo si es host/admin
+        if (profile.isHost) {
+          const botonE621 = document.getElementById("Dirigir_a_e621Index");
+          if (botonE621) {
+            botonE621.style.display = "inline-block"; // o "block" si no es botón inline
+          }
+        }
+      } catch (err) {
+        console.error("Error cargando perfil de usuario:", err);
+      }
+    }
+  });
